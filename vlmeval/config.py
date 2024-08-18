be86b0ec60e668eafaa872287232885a5de888b4
YeLuoSuiYou <zilyuye@foxmail.com>
2024-08-18 17:20:50 +0800

    update

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -207,7 +207,7 @@ mantis_series = {
 supported_VLM = {
     "MapleMLLM": partial(
         ContinuousLVLMEval,
-        pretrained_model_path="/yezilyu/code/MLLM_Train/debug_out/vqa_llava_sharegpt_2ffn_20240813_031202/checkpoint-10000/pytorch_model.bin",
+        pretrained_model_path="/yezilyu/code/MLLM_Train/debug_out/vqa_llava_sharegpt_enable_2ffn_20240815_133241/checkpoint-5000/pytorch_model.bin",
     )
 }
 
